ac304a863eaf70d3e0c933e3e0676ef0c8da8e67
Lê Viết Mẫn <manleviet@gmail.com>
2021-08-23 23:06:34 +0200

    Update copyright

--- src/main/java/at/tugraz/ist/ase/common/DiagnosisUtils.java
+++ src/main/java/at/tugraz/ist/ase/common/DiagnosisUtils.java
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2021
  *
- *  @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
  */
 
 package at.tugraz.ist.ase.common;
---
+++ src/main/java/at/tugraz/ist/ase/common/RandomGaussian.java
@@ -0,0 +1,12 @@
+/*
+ * CommonPackage
+ *
+ * Copyright (c) 2021
+ *
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ */
+
+package at.tugraz.ist.ase.common;
+
+public class RandomGaussian {
+}
