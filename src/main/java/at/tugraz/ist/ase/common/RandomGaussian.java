7843c06c3b340ad9eee8b7c106deb781150489e4
Lê Viết Mẫn <manleviet@gmail.com>
2021-08-23 23:21:31 +0200

    Add RandomGaussian

--- src/main/java/at/tugraz/ist/ase/common/RandomGaussian.java
+++ src/main/java/at/tugraz/ist/ase/common/RandomGaussian.java
@@ -8,5 +8,50 @@
 
 package at.tugraz.ist.ase.common;
 
+import java.util.Random;
+
+/**
+ * Generate pseudo-random floating point values, with an approximately Gaussian
+ * (normal) distribution.
+ */
 public class RandomGaussian {
+    double mean;
+    double variance;
+    int min;
+    int max;
+    private final Random fRandom = new Random();
+
+    /**
+     * Creates a random number
+     * @param mean mean
+     * @param variance variance of distribution
+     * @param min minbound
+     * @param max maxbound
+     */
+    public RandomGaussian(double mean, double variance, int min, int max) {
+        this.mean = mean;
+        this.variance = variance;
+        this.min = min;
+        this.max = max;
+    }
+
+    /**
+     * Gets an integer-converted value
+     * @return a random value
+     */
+    public double getGaussian() {
+        double value = this.mean + fRandom.nextGaussian() * this.variance;
+        int result = (int)Math.round(value);
+        if (result < min) result = min;
+        if (result > max) result = max;
+        return result;
+    }
+
+    public static int getRandom(int min, int max) {
+        double mean = min + (double)(max - min) / 2;
+        double variance = Math.min(2.0, (double)(max - min) / 3);
+        RandomGaussian r = new RandomGaussian(mean, variance, min, max);
+
+        return (int)r.getGaussian();
+    }
 }
---
+++ src/test/java/at/tugraz/ist/ase/common/RandomGaussianTest.java
@@ -0,0 +1,46 @@
+/*
+ * CommonPackage
+ *
+ * Copyright (c) 2021
+ *
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ */
+
+package at.tugraz.ist.ase.common;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RandomGaussianTest {
+
+    @Test
+    public void testGetGaussian() {
+        RandomGaussian gaussian = new RandomGaussian(7,2,2,12);
+        Map<Integer, Integer> valueCount = new HashMap<Integer, Integer>();
+        for (int idx = 1; idx <= 1000; ++idx) {
+            int val = (int) gaussian.getGaussian();
+            valueCount.merge(val, 1, Integer::sum);
+        }
+        System.out.println(valueCount);
+
+        for (int key = 2; key <= 12; key++) {
+            assert valueCount.containsKey(key);
+        }
+    }
+
+    @Test
+    public void testGetGaussian1() {
+        Map<Integer, Integer> valueCount = new HashMap<Integer, Integer>();
+        for (int idx = 1; idx <= 1000; ++idx) {
+            int val = RandomGaussian.getRandom(2, 12);
+            valueCount.merge(val, 1, Integer::sum);
+        }
+        System.out.println(valueCount);
+
+        for (int key = 2; key <= 12; key++) {
+            assert valueCount.containsKey(key);
+        }
+    }
+}
