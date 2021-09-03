38c869341dd326c594676ac5e6bb13b424b29510
Lê Viết Mẫn <manleviet@gmail.com>
2021-09-03 22:57:08 +0200

    deprecate RandomGaussian

--- src/main/java/at/tugraz/ist/ase/common/RandomGaussian.java
+++ src/main/java/at/tugraz/ist/ase/common/RandomGaussian.java
@@ -14,6 +14,7 @@ import java.util.Random;
  * Generate pseudo-random floating point values, with an approximately Gaussian
  * (normal) distribution.
  */
+@Deprecated
 public class RandomGaussian {
     double mean;
     double variance;
