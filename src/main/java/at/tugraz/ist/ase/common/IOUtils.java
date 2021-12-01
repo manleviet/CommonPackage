9b72410e88bd61f265d9e5384766b901a69aae9f
Lê Viết Mẫn <manleviet@gmail.com>
2021-12-01 16:43:54 +0100

    Rename Utils to IOUtils

--- src/main/java/at/tugraz/ist/ase/common/IOUtils.java
+++ src/main/java/at/tugraz/ist/ase/common/IOUtils.java
@@ -18,7 +18,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
-public class Utils {
+public final class IOUtils {
     /**
      * Gets InputStream of a file in the resource
      * @param classLoader the classLoader of the program
