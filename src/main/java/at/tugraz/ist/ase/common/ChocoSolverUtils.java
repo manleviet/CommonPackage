0c286a10fc4e650b59e661401fc99c3e54a66f7e
Lê Viết Mẫn <manleviet@gmail.com>
2021-12-07 12:56:42 +0100

    New class ChocoSolverUtils

---
+++ src/main/java/at/tugraz/ist/ase/common/ChocoSolverUtils.java
@@ -0,0 +1,32 @@
+/*
+ * CommonPackage
+ *
+ * Copyright (c) 2021
+ *
+ * @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+ */
+
+package at.tugraz.ist.ase.common;
+
+import lombok.experimental.UtilityClass;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.Variable;
+
+@UtilityClass
+public class ChocoSolverUtils {
+
+    /**
+     * Get a variable with the given name.
+     * @param model a Choco model
+     * @param name the name of the variable
+     * @return a {@link Variable} with the given name, or null if no such variable exists
+     */
+    private Variable getVariable(Model model, String name) {
+        for (Variable v : model.getVars()) {
+            if (v.getName().equals(name)) {
+                return v;
+            }
+        }
+        return null;
+    }
+}
